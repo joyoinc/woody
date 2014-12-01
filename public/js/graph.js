933446965a22a491b6a7fffc19daba1a6214c7f6
Yuan Zhi <yuzhi@microsoft.com>
2014-12-01 10:51:26 -0500

    ..

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,19 +1,28 @@
 
 (function(){
 
-  var GraphVertex = function() { 
+  // ctor 
+  // param (vertexId, [configs])
+  var GraphVertex = function(vertexId) { 
     var default_config = {
       label : "default_Label",
       color : "#777",
-      pos : {x:0,y:0},
-      vertexId: 0
+      pos : {x:0,y:0}
     }
     var configs = {};
-    if(arguments[0]) configs = arguments[0];
+    if(arguments[arguments.length-1]) configs = arguments[arguments.length-1];
     for(var idx in default_config)
       if(typeof configs[idx] == 'undefined') configs[idx] = default_config[idx];
 
-    this.initialize(configs); 
+    this.set({
+      configs: configs,
+      vertexId: vertexId,
+      cursor: "pointer",
+      alpha: 0.5,
+      name: "wfGraph_vertex"
+    });
+
+    this.initialize(); 
   }
 
   var p = GraphVertex.prototype = new createjs.Container(); // inherit from Container
@@ -21,36 +30,38 @@
   p.background;
   p.width;
   p.height;
-  //p.edges = [];
 
   GraphVertex.prototype.Container_initialize = p.initialize;
 
-  GraphVertex.prototype.initialize = function(configs) {
+  GraphVertex.prototype.initialize = function() {
     this.Container_initialize();
 
-    this.set({
-      x: configs.pos.x, 
-      y: configs.pos.y,
-      label: configs.label,
-      vertexId: configs.vertexId,
-      cursor: "pointer",
-      alpha: 0.5,
-      name: "wfGraph_vertex"
+    this.addChild(new createjs.Shape(), new createjs.Text);
+    this.paint();
+  }
+
+  p.paint = function() {
+    var text = this.getChildAt(1);
+    text.set({
+      text: this.configs.label,
+      font: "20px Arial",
+      color: "#000",
+      textBaseline: "top",
+      textAlign: "center",
+      y: -10
     });
 
-    var text = new createjs.Text(this.label, "20px Arial", "#000");
-    text.textBaseline = "top";
-    text.textAlign = "center";
+    var width = text.getMeasuredWidth()+30;
+    var height = text.getMeasuredHeight()+20;
 
-    this.width = text.getMeasuredWidth()+30;
-    this.height = text.getMeasuredHeight()+20;
+    var background = this.getChildAt(0);
+    background.graphics.clear().beginFill(this.configs.color)
+    .drawRoundRect(0-width/2,0-height/2,width,height,10);
 
-    this.background = new createjs.Shape();
-    this.background.graphics.beginFill(configs.color)
-    .drawRoundRect(0-this.width/2,0-this.height/2,this.width,this.height,10);
-
-    text.y = -10;
-    this.addChild(this.background,text);
+    this.set({
+      x: this.configs.pos.x,
+      y: this.configs.pos.y
+    });
   }
 
   p.dragHandler = function(evt) {
@@ -60,19 +71,11 @@
 
   p.dropHandler = function(evt) {
     //console.log(this.edges);
-  }
-
-  p.addEdge = function(graphEdge) {
-    var elem = null;
-    var array = this.edges;
-    if(graphEdge==null)
-      return;
-    for(var i=0; i<array.length; i++) {
-      if(array[i]==graphEdge) {
-        return;
+    stage.children.forEach(function(elem){
+      if(elem.name=="wfGraph_edge") {
+        elem.paint();
       }
-    }
-    array.push(graphEdge);
+    });
   }
 
   window.GraphVertex = GraphVertex;
@@ -80,57 +83,65 @@
 
 (function(){
 
-  var GraphEdge = function(sourceVertex, sinkVertex) { 
+  // ctor 
+  // param (sourceGraphVertex, sinkGraphVertex, [configs])
+  var GraphEdge = function(sourceVertex, sinkVertex) {
     var default_config = {
-      startX: sourceVertex.x,
-      startY: sourceVertex.y,
-      endX: sinkVertex.x,
-      endY: sinkVertex.y,
-      sourceId: sourceVertex.vertexId,
-      sinkId: sinkVertex.vertexId,
       color : "#00f"
     }
     var configs = {};
-    if(arguments[2]) configs = arguments[2];
+    if(arguments[arguments.length-1]) configs = arguments[arguments.length-1];
     for(var idx in default_config)
       if(typeof configs[idx] == 'undefined') configs[idx] = default_config[idx];
 
-    this.initialize(configs); 
+    this.set({
+      sourceVertex: sourceVertex,
+      sinkVertex: sinkVertex,
+      configs: configs,
+      cursor: "pointer",
+      alpha: 0.3,
+      name: "wfGraph_edge"
+    });
 
-    console.log(sourceVertex.label + "->" + sinkVertex.label);
+    this.initialize(); 
+
+    console.log(sourceVertex.configs.label + "->" + sinkVertex.configs.label);
   }
 
   var p = GraphEdge.prototype = new createjs.Container(); // inherit from Container
 
   GraphEdge.prototype.Container_initialize = p.initialize;
 
-  GraphEdge.prototype.initialize = function(configs) {
+  GraphEdge.prototype.initialize = function() {
     this.Container_initialize();
 
-    this.set({
-      x: configs.endX,
-      y: configs.endY,
-      sourceId: configs.sourceId,
-      sinkId: configs.sinkId,
-      cursor: "pointer",
-      alpha: 0.3,
-      name: "wfGraph_edge"
-    });
+    this.addChild(new createjs.Shape());
+    this.paint();
+  }
 
-    var diffX = configs.endX - configs.startX;
-    var diffY = configs.endY - configs.startY;
+  p.paint = function() {
+    var arrow = this.getChildAt(0);
+
+    var startX = this.sourceVertex.x;
+    var startY = this.sourceVertex.y;
+    var endX = this.sinkVertex.x;
+    var endY = this.sinkVertex.y;
+    var diffX = endX - startX;
+    var diffY = endY - startY;
     var radian = Math.atan2(diffY , diffX);
     var len = Math.sqrt(diffY*diffY + diffX*diffX);
 
-    var arrow = new createjs.Shape();
-    arrow.graphics.s(configs.color)
+    arrow.graphics.clear().s(this.configs.color)
     .mt(0,0).lt(-len,0)
     .mt(-len/2,0).lt(-len/2-5,5)
     .mt(-len/2,0).lt(-len/2-5,-5);
     var degree = radian / Math.PI * 180;
     arrow.rotation = degree;
 
-    this.addChild(arrow);
+    this.set({
+      x: endX,
+      y: endY
+    });
   }
 
   window.GraphEdge = GraphEdge;
@@ -138,38 +149,40 @@
 
 function startDrawEdgeHandler (evt) {
   sourceVertex = evt.currentTarget;
-  connection = new createjs.Shape().set({
-x: evt.currentTarget.x,
-y: evt.currentTarget.y,
-graphics: new createjs.Graphics().s("#0f0").dc(0,0,10)
-});
-stage.addChild(connection);
-stage.addEventListener("stagemousemove", tmpDrawEdgeHandler);
-stage.addEventListener("stagemouseup", endDrawEdgeHandler);
+  stage.addChild(new createjs.Shape().set({
+    x: evt.currentTarget.x,
+    y: evt.currentTarget.y,
+    graphics: new createjs.Graphics().s("#0f0").dc(0,0,10),
+    name: "tmpEdge"
+  }));
+  stage.addEventListener("stagemousemove", tmpDrawEdgeHandler);
+  stage.addEventListener("stagemouseup", endDrawEdgeHandler);
 }
 
 function tmpDrawEdgeHandler (evt) {
+  var connection = stage.getChildByName("tmpEdge");
   connection.graphics.clear().s('#0f0').mt(0,0)
-    .lt(stage.mouseX-connection.x, stage.mouseY-connection.y);
+  .lt(stage.mouseX-connection.x, stage.mouseY-connection.y);
 }
 
 function endDrawEdgeHandler (evt) {
-  var targetVertex , targets = stage.getObjectsUnderPoint(stage.mouseX, stage.mouseY);
+  var sinkVertex , targets = stage.getObjectsUnderPoint(stage.mouseX, stage.mouseY);
   for(var i=0; i<targets.length; i++) {
     if(targets[i].parent!=null && targets[i].parent.name=="wfGraph_vertex") {
-      targetVertex = targets[i].parent; 
+      sinkVertex = targets[i].parent; 
       break;
     }
   }
 
+  var connection = stage.getChildByName("tmpEdge");
   stage.removeChild(connection);
-  if(targetVertex != null) {
+  if(sinkVertex != null) {
     var sourceId = sourceVertex.vertexId;
-    var sinkId = targetVertex.vertexId;
+    var sinkId = sinkVertex.vertexId;
     if(hasEdge(edges, sourceId, sinkId)) {
       console.log(sourceId + "->" + sinkId + ": exists!");
     } else {
-      drawEdge(sourceId, sinkId);
+      drawGraphEdge(sourceId, sinkId);
       edges.push({source:sourceId, sink:sinkId});
     }
   } 
@@ -186,7 +199,7 @@ function hasEdge(edgeArray, sourceId, sinkId) {
   return false;
 }
 
-function drawEdge(sourceId, sinkId) {
+function drawGraphEdge(sourceId, sinkId) {
   var source,sink = null;
   for(var j=0; j<stage.children.length; j++) {
     if(stage.children[j].name=="wfGraph_vertex") {
@@ -201,47 +214,48 @@ function drawEdge(sourceId, sinkId) {
   }
 
   if(source!=null && sink!=null) {
-    var edge = stage.addChild(new GraphEdge(source, sink));
-    //source.addEdge(edge); sink.addEdge(edge);
+    stage.addChild(new GraphEdge(source, sink));
   }
 }
+
+function drawGraphVertex(vertexId, label, pos) {
+  stage.addChild(new GraphVertex(vertexId, {label: label, pos:pos}));
+}
+
 $(document).ready(function(){
-    var toggleBtn = function(btn){ 
+  var toggleBtn = function(btn){ 
     $('.btn.active').removeClass('active');
-    if(btn)
-    $(btn).addClass('active');
-    };
+    if(btn) $(btn).addClass('active');
+  };
 
-    $('.btn#save').click(function(){ 
-      toggleBtn();
-      });
+  $('.btn#save').click(function(){ toggleBtn(); });
 
-    $('.btn#editEdge').click(function(){ 
-      var btn = $(this);
-      if($(btn).hasClass('active')) return;
+  $('.btn#editEdge').click(function(){ 
+    var btn = $(this);
+    if($(btn).hasClass('active')) return;
 
-      stage.children.forEach(function(elem){
-        if(elem.name=="wfGraph_vertex") {
+    stage.children.forEach(function(elem){
+      if(elem.name=="wfGraph_vertex") {
         elem.removeAllEventListeners();
         elem.on("mousedown", startDrawEdgeHandler);
-        }
-        });
+      }
+    });
 
-      toggleBtn(btn);
-      });
+    toggleBtn(btn);
+  });
 
-    $('.btn#editVertex').click(function(){ 
-        var btn = $(this);
-        if($(btn).hasClass('active')) return;
+  $('.btn#editVertex').click(function(){ 
+    var btn = $(this);
+    if($(btn).hasClass('active')) return;
 
-        stage.children.forEach(function(elem){
-          if(elem.name=="wfGraph_vertex") {
-          elem.removeAllEventListeners();
-          elem.on("pressmove", elem.dragHandler);
-          elem.on("pressup", elem.dropHandler);
-          }
-          });
+    stage.children.forEach(function(elem){
+      if(elem.name=="wfGraph_vertex") {
+        elem.removeAllEventListeners();
+        elem.on("pressmove", elem.dragHandler);
+        elem.on("pressup", elem.dropHandler);
+      }
+    });
 
-        toggleBtn(btn);
-        });
+    toggleBtn(btn);
+  });
 });
